74d124479bc1a6124ea0c0600ec3ad38a6158796
Max <161768135+Picoodi@users.noreply.github.com>
2024-11-23 22:34:44 +0100

    Add files via upload

---
+++ js-calculator/index.js
@@ -0,0 +1,22 @@
+const Display = document.getElementById("Display")
+
+function ShowOnDisplay(input){
+    Display.value += input;
+
+}
+
+function clearDisplay(){
+    Display.value = "";
+
+}
+
+
+function calculate(){
+    try{
+        Display.value = eval(Display.value);
+    }
+    catch(error){
+        Display.value = "Error";
+    }
+
+}
